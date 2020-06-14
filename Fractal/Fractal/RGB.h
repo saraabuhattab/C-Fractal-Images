9e9b63a6e2176e7816215a42c181ca7debb6f7ae
saraabuhattab <saraabuhattab99@gmail.com>
2020-06-14 04:53:44 -0400

    Fractal Images Project

---
+++ Fractal/Fractal/RGB.h
@@ -0,0 +1,21 @@
+
+#ifndef RGB_H_
+#define RGB_H_
+
+namespace caveofprogramming {
+
+	struct RGB {
+		double r;
+		double g;
+		double b;
+
+		RGB(double r, double g, double b);
+	};
+
+	RGB operator-(const RGB& first, const RGB& second);
+
+} /* namespace caveofprogramming */
+
+
+
+#endif /* RGB_H_ */
